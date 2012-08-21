6cc920bfdd5818d42cc2adeef82bc2be9c146a33
Alex Abd <Alexander.Abdugafarov@nitka.com>
2012-08-22 01:42:09 +0500

    Bugfix: NotSerializableException on drag'n'drop

--- src/org/dotahq/util/DragAndDropUtil.groovy
+++ src/org/dotahq/util/DragAndDropUtil.groovy
@@ -5,10 +5,10 @@ import java.awt.datatransfer.DataFlavor;
 class DragAndDropUtil {
 	
 	public static DataFlavor createDataFlavor(Object data){
-		return new DataFlavor(data.getClass(), 'application/x-java-serialized-object')
+		return createDataFlavor(data.getClass())
 	}
 
     public static DataFlavor createDataFlavor(Class dataClass){
-        return new DataFlavor(dataClass, 'application/x-java-serialized-object')
+        return new DataFlavor("${DataFlavor.javaJVMLocalObjectMimeType}; class=${dataClass.canonicalName}")
     }
 }
