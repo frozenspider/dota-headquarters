25c53c5c8b2dbe8e864ae6fcf5517906360fb9f7
Alex Abd <Alexander.Abdugafarov@nitka.com>
2012-09-02 23:38:00 +0500

    Changed absolute to local path is ScriptPath

--- playdota/src/org/dotahq/playdota/ScriptPath.groovy
+++ playdota/src/org/dotahq/playdota/ScriptPath.groovy
@@ -1,7 +1,5 @@
 package org.dotahq.playdota
 
 class ScriptPath {
-	public static final File folder = new File(
-		"F:\\_My\\git\\DotAHeadquarters\\playdota-parsing-result"
-	)
+	public static final File folder = new File("playdota-parsing-result")
 }
